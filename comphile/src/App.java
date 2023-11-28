0991cb07105897377b3027d9a109eebb50f9b555
Caeles12 <pol.jaouen@gmail.com>
2023-11-28 11:07:11 +0100

    Début pattern Visiteur
    - Association des tokens aux lambda fonctions
    - Visite de l'AST
    
    Co-Authored-By: feesh8 <133744318+feesh8@users.noreply.github.com>

--- comphile/src/App.java
+++ comphile/src/App.java
@@ -1,3 +1,5 @@
+import java.util.function.Consumer;
+
 import org.antlr.runtime.*;
 import org.antlr.runtime.tree.CommonTree;
 
@@ -18,7 +20,32 @@ public class App {
 
     WhileParser.program_return program = parser.program();
     CommonTree tree = (CommonTree) program.getTree();
-    System.out.println(tree.toStringTree());
-  }
 
+    Visitor visitor = new Visitor();
+
+    // Visitor Lambdas
+    Consumer<CommonTree> function_lambda = (CommonTree t) -> {
+      String function_name = t.getChild(0).getText();
+      System.out.println("Function " + function_name);
+      CommonTree definition = (CommonTree) t.getChild(1);
+      visitor.visit(definition);
+    };
+
+    Consumer<CommonTree> function_definition_lambda = (CommonTree t) -> {
+      System.out.println("Definition");
+      CommonTree input = (CommonTree) t.getChild(0);
+      CommonTree commands = (CommonTree) t.getChild(1);
+      CommonTree output = (CommonTree) t.getChild(1);
+      visitor.visit(input);
+      visitor.visit(commands);
+      visitor.visit(output);
+    };
+
+    // Assign lambdas to tokens
+    visitor.assign(WhileParser.FUNCTION, function_lambda);
+    visitor.assign(WhileParser.DEFINITION, function_definition_lambda);
+
+    // Visit the AST
+    visitor.visit(tree);
+  }
 }
