7b90d5f0853d434219fa296449a54e310ae04481
Pandadorable <geffroy250@gmail.com>
2024-01-16 14:44:11 +0100

    move jar

--- compilateur_sans_echec.sh
+++ compilateur_sans_echec.sh
@@ -2,7 +2,7 @@ echo "Compilation sans echec..."
 cd comphile/
 ./run_and_build.sh
 cd ../
-cp comphile/target/comphile-1.0-SNAPSHOT-jar-with-dependencies.jar lib/compilateur.jar
+mv comphile/target/comphile-1.0-SNAPSHOT-jar-with-dependencies.jar lib/compilateur.jar
 cd lib/
 ./run.sh ${@:1}
 cd ../
